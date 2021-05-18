629140440debfc0625c2ceecadc26f22f759bbaf
Danny Rivera <samboard@hotmail.com>
2021-05-18 14:13:21 -0400

    commit tree 7cf26018136ca4ccbbc5dd0121a9350c804547d4

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+// Write a Function To Case-insensitively Match strings
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+}
+
+// Write a Function To Partially Match Substrings
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+}
+
+// Write a Function To Match object Values To a Provided string
+function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+}
